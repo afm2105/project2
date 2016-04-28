e70af25d8b928d789f6271f73347fd62133b13a6
antony mastroniani <afm2105@gmail.com>
2016-04-28 10:35:43 -0400

    updates to use PG

---
+++ coaching_facts/app/controllers/coaches_controller.rb
@@ -0,0 +1,21 @@
+class CoachesController < ApplicationController
+  def show
+    @coach = Coach.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    # render plain: params [:coach].inspect
+    @coach = Coach.new(params.require(:coach).permit(:name, :tidbits))
+
+    @coach.save
+    redirect_to @coach
+  end
+
+  private
+    def coach_params
+      params.require(:coach).permit(:name, :tidbits)
+    end
+end
